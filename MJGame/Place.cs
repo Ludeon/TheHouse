49e872e553d26b811fb5bbde9dd6474175d6336a
Tynan <tynan.sylvester@gmail.com>
2014-07-01 23:07:47 -0400

    Created first place class and child.

---
+++ MJGame/Place.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJGame
+{
+	abstract class Place
+	{
+		protected string introText;
+
+
+		protected abstract void HandleInput(string inp);
+
+		public void Arrived()
+		{
+			Console.Clear();
+			Console.WriteLine(introText);
+			
+			string inp;
+			inp = Program.GetInput();
+			
+			HandleInput( inp );
+		}
+
+		
+
+
+	}
+
+
+	class Place_frontHouse : Place
+	{
+		public Place_frontHouse()
+		{
+			introText= "You are standing in front of a rural house. It is ramshackle and shitty-looking.";
+		}
+
+		protected override void HandleInput(string inp)
+		{
+			
+			if (inp == "open door" || inp == "open" || inp == "go in")
+			{
+				//PAGE 3a
+				Console.Clear();
+				Console.WriteLine("A large dog is sitting in the entrance. He looks at you and says, \"What do you want, fucka?\"");
+
+				string inp3a = Program.GetInput();
+
+				if (inp3a == "greet kindly" || inp3a == "say hello")
+				{ Console.WriteLine(" Get out of my house you ape!");
+
+				}
+				else if (inp3a == "give biscuit")
+				{//PAGE 4
+				Console.Clear();
+				Console.WriteLine(" The dog catches the buiscuit you tossed him, munches quietly and promptly dies on the carpet. \n You proceed down the hall.");
+				}
+			
+			}
+			if (inp == "go to the yard")
+			{
+				//PAGE 3b
+				Console.Clear();
+				Console.WriteLine("A collection of cement dinosaurs stare balefully back at you from the yard.\n\nThey terrify you. You decide to enter the house.");
+				Console.ReadKey();
+				Arrived();
+			}
+		}
+	}
+
+
+}
--- MJGame/Program.cs
+++ MJGame/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace MJGame
 {
-	class Program
+	static class Program
 	{
-		static string[] inventory = new string[10];
+		public static string[] inventory = new string[10];
 
 		static void Initialize() 
 		{
@@ -23,7 +23,7 @@ namespace MJGame
 			inventory[8] = " Manet art work";
 	
 		}
-
+		
 		static void Main(string[] args)
 		{
 			Initialize();
@@ -36,44 +36,13 @@ namespace MJGame
 			Console.WriteLine("Press a key to begin.");
 			Console.ReadKey();
 
-			//PAGE 2
-			page2:
-			Console.Clear();
-
-			Console.WriteLine("You are standing in front of a rural house. It is ramshackle and shitty-looking.");
-
-			string inp;
-			inp = GetInput();
-
-			if (inp == "open door" || inp == "open" || inp == "go in")
-			{
-				//PAGE 3a
-				Console.Clear();
-				Console.WriteLine("A large dog is sitting in the entrance. He looks at you and says, \"What do you want, fucka?\"");
-
-				string inp3a = GetInput();
-
-				if (inp3a == "greet kindly" || inp3a == "say hello")
-				{ Console.WriteLine(" Get out of my house you ape!");
-				}
 
 
-				if (inp3a == "give biscuit")
-				{//PAGE 4
-				Console.Clear();
-				Console.WriteLine(" The dog catches the buiscuit you tossed him, munches quietly and promptly dies on the carpet. \n You proceed down the hall.");
-				}
-			
-			}
-			if (inp == "go to the yard")
-			{
-				//PAGE 3b
-				Console.Clear();
-				Console.WriteLine("A collection of cement dinosaurs stare balefully back at you from the yard.\n\nThey terrify you. You decide to enter the house.");
-				Console.ReadKey();
-				goto page2;
-			}
-			
+			Place_frontHouse fh;
+			fh = new Place_frontHouse();
+			fh.Arrived();
+	
+						
 
 			Console.ReadKey();
 		}
@@ -84,7 +53,7 @@ namespace MJGame
 		//=================================================
 
 
-		static string GetInput()
+		public static string GetInput()
 		{
 			int numBadInputs = 0;
 
@@ -110,35 +79,36 @@ namespace MJGame
 
 			if (playerInput == "inventory")
 			{
-				Writeinventory();
+				InvUtil.Writeinventory();
+
+
 				goto start;
 			}
 
 			return playerInput;
 		}
+	}
 
-		static void Writeinventory()
+
+
+
+
+	public static class InvUtil
+	{
+		public static void Writeinventory()
 		{
 			int invcount = 0;
 
 			loopstart:
 
-			Console.WriteLine(inventory[invcount]);
+			Console.WriteLine(Program.inventory[invcount]);
 			invcount = invcount + 1;
 			if (invcount == 9) return; 
 			goto loopstart;
 
 		}
-
-
 	}
 }
-
-
-
-
-
-
 
 
 
